5426167048b208e011bce06278919c67022c42f9
Divya <accio112>
2018-10-30 19:51:50 +0530

    Add new msg and create msg events

---
+++ public/js/index.js
@@ -0,0 +1,27 @@
+var socket = io();//req from client to server to make a connection and keep it open
+
+socket.on('connect', function(){
+    console.log('connected to server');
+
+    socket.emit('createEmail', {
+        to:'divi@gmail.com',
+        text: "doing just good"
+    })
+
+    socket.emit('createMessage', {
+        to:'divi@gmail.com',
+        text: "new message creation"
+    })
+});
+
+socket.on('disconnect', function(){
+    console.log('Disconnected from server');
+});
+
+socket.on('newEmail', function (email) {
+    console.log('new email', email);
+})
+
+socket.on('newMessage', function (msg) {
+    console.log('New message arrived', msg);
+})
--- server/server.js
+++ server/server.js
@@ -1,11 +1,46 @@
 const path = require('path');
+const http =  require('http');
 const express = require('express');
+const socketIO =  require('socket.io');
 const publicPath = path.join(__dirname, '../public');
 
 const port = process.env.PORT ||3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection', (socket)=>{
+    console.log('New user connected');
+
+    //create event
+    socket.emit('newEmail', {
+        from: 'mike@gmail.com',
+        text: "how u doin?",
+        createdAt: 124
+    });
+
+    socket.emit('newMessage', {
+        from: 'divya',
+        text: 'blah',
+        createdAt: 123
+    })
+
+    //event listener
+    socket.on('createEmail', (newEmail)=>{
+        console.log('Create email', newEmail);
+    })
+
+    socket.on('createMessage', (newMsg)=>{
+        console.log('Create msg', newMsg);
+    })
+    socket.on('disconnect', ()=>{
+        console.log('Disconnected');
+    })
+
+});
+
+server.listen(port,()=>{
     console.log(`Server is up on ${port}`);
 })
